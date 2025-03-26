49286d7b5e2c8e2b17a1da7b7dfd19d7c2a923a7
cuong404vn <nguyencuong.contact.work@gmail.com>
2025-03-26 14:56:56 +0700

    Fix lỗi replay

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,26 +2,33 @@
 
 public class PlayerCollision : MonoBehaviour
 {
-    private Gamemanager gamemanager;
 
-    private void Awake()
-    {
-        gamemanager = FindAnyObjectByType<Gamemanager>();
-    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+      
+        GameManager gameManager = GameManager.Instance;
+
+    
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager not found in OnTriggerEnter2D!");
+            return;
+        }
+
         if (collision.CompareTag("Coin"))
         {
             Destroy(collision.gameObject);
-            gamemanager.AddScore(1);
+            gameManager.AddCoins(1);
+            Debug.Log("Coin collected! Total coins: " + gameManager.coins);
         }
         else if (collision.CompareTag("Trap"))
         {
-            gamemanager.GameOver();
+          
         }
         else if (collision.CompareTag("Enemy"))
         {
-            gamemanager.GameOver();
+           
         }
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI coinsText;
+    public TextMeshProUGUI keysText;
+
+    void Start()
+    {
+        
+        if (coinsText == null)
+            coinsText = GameObject.Find("CoinsText")?.GetComponent<TextMeshProUGUI>();
+
+        if (keysText == null)
+            keysText = GameObject.Find("KeysText")?.GetComponent<TextMeshProUGUI>();
+
+        
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.OnCoinsChanged += UpdateCoinsUI;
+            GameManager.Instance.OnKeysChanged += UpdateKeysUI;
+
+            
+            UpdateCoinsUI(GameManager.Instance.coins);
+            UpdateKeysUI(GameManager.Instance.keys);
+        }
+    }
+
+    void OnDestroy()
+    {
+        
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.OnCoinsChanged -= UpdateCoinsUI;
+            GameManager.Instance.OnKeysChanged -= UpdateKeysUI;
+        }
+    }
+
+    void UpdateCoinsUI(int value)
+    {
+        if (coinsText != null)
+            coinsText.text = value.ToString();
+        else
+            Debug.LogWarning("CoinsText is null!");
+    }
+
+    void UpdateKeysUI(int value)
+    {
+        if (keysText != null)
+            keysText.text = value.ToString();
+        else
+            Debug.LogWarning("KeysText is null!");
+    }
+}
