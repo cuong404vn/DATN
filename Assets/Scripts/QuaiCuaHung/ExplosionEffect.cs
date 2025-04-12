0720180363aee0a87a338de94410aa0136397be4
hhoanghung <hungh2472@gmail.com>
2025-04-12 20:44:34 +0700

    Update quái chém và âm thanh map

---
+++ Assets/Scripts/QuaiCuaHung/ExplosionEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ExplosionEffect : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        if (ps != null)
+        {
+            Destroy(gameObject, ps.main.duration); // Hủy sau khi hiệu ứng kết thúc
+        }
+    }
+}
