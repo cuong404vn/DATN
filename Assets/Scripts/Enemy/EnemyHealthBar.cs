74a3ee9b5320a0a538ecbeb4a89554f193e6dbbb
cuong404vn <nguyencuong.contact.work@gmail.com>
2025-05-02 23:49:04 +0700

    Update EnemyHealthBar

---
+++ Assets/Scripts/Enemy/EnemyHealthBar.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 0.5f, 0); 
+
+    public Image fillImage;
+    public Text nameText; 
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+   
+        transform.position = target.position + offset;
+
+     
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+    }
+
+    public void SetHealth(int current, int max)
+    {
+        if (fillImage != null)
+            fillImage.fillAmount = (float)current / max;
+    }
+
+    public void SetName(string name)
+    {
+        if (nameText != null)
+            nameText.text = name;
+    }
+}
